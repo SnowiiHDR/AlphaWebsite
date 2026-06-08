98648559f2c54803559f54f08f57a7223b93f727
Zion Singleton <zs394@cornell.edu>
2026-06-07 23:06:46 -0400

    First changes w/ vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alpha-chapter.example.com", // TODO: replace with real domain
+  site: "https://alpha-website-nu.vercel.app/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
